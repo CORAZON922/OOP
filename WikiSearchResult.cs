eaab39d9b571e5722778fa7ed83d259382efe6b9
CORAZON922 <Sevolod228@gmail.com>
2025-11-26 20:45:27 +0300

    Update WikiSearchResult.cs

--- WikiSearchResult.cs
+++ WikiSearchResult.cs
@@ -1,22 +1,15 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 public class WikiSearchResult
 {
     public Query query { get; set; }
 
-    /// <summary>
-    /// Проверяет, есть ли результаты поиска
-    /// </summary>
     public bool HasResults => query?.search != null && query.search.Length > 0;
 
-    /// <summary>
-    /// Возвращает количество найденных статей
-    /// </summary>
+
     public int ResultsCount => query?.search?.Length ?? 0;
 
-    /// <summary>
-    /// Возвращает статью по индексу
-    /// </summary>
+   
     public SearchItem GetArticle(int index)
     {
         if (query?.search == null || index < 0 || index >= query.search.Length)
@@ -36,20 +29,10 @@ public class SearchItem
 {
     public string title { get; set; }
     public int pageid { get; set; }
-    public string snippet { get; set; }
 
-    /// <summary>
-    /// Возвращает URL статьи в Википедии
-    /// </summary>
+
+   
     public string GetArticleUrl() => $"https://ru.wikipedia.org/w/index.php?curid={pageid}";
 
-    /// <summary>
-    /// Возвращает очищенный от HTML-тегов текст сниппета
-    /// </summary>
-    public string GetCleanSnippet() => Regex.Replace(snippet ?? "", "<.*?>", string.Empty);
-
-    /// <summary>
-    /// Возвращает форматированную информацию о статье
-    /// </summary>
     public string GetFormattedInfo(int index) => $"{index}. {title} (ID: {pageid})";
 }
